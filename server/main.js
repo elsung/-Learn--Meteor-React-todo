2807fb25beed04ffc2d267d484394e238c010fb9
Eric Sung <elsung@gmail.com>
2016-09-27 00:13:03 -0700

    halfway through tut, but run iOS isnt working

--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
+// imports the API tasks module on the server
+import '../imports/api/tasks.js';
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
